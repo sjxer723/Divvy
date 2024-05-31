c8e8cee368ebaaef3aa9a851ca44a02b1cacf3dd
Jiaxin Song <sjtu_xiaosong@sjtu.edu.cn>
2024-05-31 23:05:30 +0800

    update valuations

--- rosette_code/ro_eg.rkt
+++ rosette_code/ro_eg.rkt
@@ -25,10 +25,19 @@
      (= (count-trues (list-ref an-alloc i)) 1))
    (range m)))
 
-(define (valuation i a-set) (count-trues a-set))
+(define valuations
+  (list (list 1 2 3 4 5)
+        (list 5 4 3 2 1)
+        (list 3 3 3 3 3)
+        (list 3 3 3 3 3)))
+
+(define (valuation i j alloc)
+  (apply + (for/list ([k (range m)])
+             (* (if (list-ref (list-ref alloc k) j) 1 0)
+                (list-ref (list-ref valuations i) k)))))
 
 (define (remove-from-set a-set j)
-  (map (λ (i) (if (= i j) #f (list-ref a-set i))) (range (length a-set))))
+  (map (λ (i) (if (= i j) (build-list n (lambda (x) #f)) (list-ref a-set i))) (range (length a-set))))
 
 (define (EF1-alloc? an-alloc)
   (andmap
@@ -38,15 +47,15 @@
         (or
          (ormap
           (λ (j)
-            (>= (valuation i1 (list-ref an-alloc i1))
-                (valuation i1
+            (>= (valuation i1 i1 an-alloc)
+                (valuation i1 i2
                            (remove-from-set
-                            (list-ref an-alloc i2)
+                            an-alloc
                             j))))
           (range m))
-         (>= (valuation i1 (list-ref an-alloc i1))
-             (valuation i1
-                        (list-ref an-alloc i2)))))
+         (>= (valuation i1 i1 an-alloc)
+             (valuation i1 i2
+                        an-alloc))))
       (range n)))
    (range n)))
 
