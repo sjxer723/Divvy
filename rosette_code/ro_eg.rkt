b660423f7a5d923ca087ca03e40ceb3c1ae281b6
Jiaxin Song <sjtu_xiaosong@sjtu.edu.cn>
2024-05-31 22:01:25 +0800

    update the definition of ef1

--- rosette_code/ro_eg.rkt
+++ rosette_code/ro_eg.rkt
@@ -25,30 +25,34 @@
      (= (count-trues (list-ref an-alloc i)) 1))
    (range m)))
 
-(define (valuation i a-set)
-  1)
+(define (valuation i a-set) (count-trues a-set))
 
-(define remove-from-set
-  #t)
+(define (remove-from-set a-set j)
+  (map (λ (i) (if (= i j) #f (list-ref a-set i))) (range (length a-set))))
 
 (define (EF1-alloc? an-alloc)
   (andmap
    (λ (i1)
      (andmap
       (λ (i2)
-        (ormap
-         (λ (j)
-           (>= (valuation i1 (list-ref an-alloc i1))
-               (valuation i1
-                          (remove-from-set
-                           (list-ref an-alloc i2)
-                           j))))
-         (range m)))))
+        (or
+         (ormap
+          (λ (j)
+            (>= (valuation i1 (list-ref an-alloc i1))
+                (valuation i1
+                           (remove-from-set
+                            (list-ref an-alloc i2)
+                            j))))
+          (range m))
+         (>= (valuation i1 (list-ref an-alloc i1))
+             (valuation i1
+                        (list-ref an-alloc i2)))))
+      (range n)))
    (range n)))
 
 (assert (feasible-alloc? alloc))
-#;(assert (EF-alloc? alloc))
+(assert (EF1-alloc? alloc))
+
 (evaluate
  alloc
  (solve #t))
-#;(define-symbolic* x integer? #:length 5)
